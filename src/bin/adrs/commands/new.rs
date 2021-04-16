4668fc50d16b3ae5220f34da88acdb78b7752267
josh rotenberg <joshrotenberg@gmail.com>
2021-04-15 18:42:17 -0700

    stubbing new

--- src/bin/adrs/commands/new.rs
+++ src/bin/adrs/commands/new.rs
@@ -1,8 +1,12 @@
 use clap::ArgMatches;
 use std::error::Error;
 
-pub fn run(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
-    println!("in new");
+pub(crate) fn run(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
+    if let Some(title) = matches.value_of("TITLE") {
+        // create a new adr from the template with the given title, incrementing the index appropriately.
+        // if any superceded ADRs are specified, update the status of those ADRs to specify that they've been superceded by the new ADR.
+        dbg!(title);
+    }
 
     Ok(())
 }
