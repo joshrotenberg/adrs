b528338c64bc3f2abe265303d948e4210611b732
josh rotenberg <joshrotenberg@gmail.com>
2024-03-06 11:22:31 -0800

    adding more tests

--- tests/test_init.rs
+++ tests/test_init.rs
@@ -1,12 +1,12 @@
 use assert_cmd::Command;
 use assert_fs::fixture::TempDir;
-use std::path::Path;
+use assert_fs::prelude::*;
 
 #[test]
 #[serial_test::serial]
 fn test_init_default() {
-    let tmp_dir = TempDir::new().unwrap();
-    std::env::set_current_dir(tmp_dir.path()).unwrap();
+    let temp = TempDir::new().unwrap();
+    std::env::set_current_dir(temp.path()).unwrap();
 
     Command::cargo_bin("adrs")
         .unwrap()
@@ -14,13 +14,11 @@ fn test_init_default() {
         .assert()
         .success();
 
-    assert!(Path::new(tmp_dir.path())
-        .join("doc/adr")
-        .join("0001-record-architecture-decisions.md")
-        .exists());
+    temp.child("doc/adr/0001-record-architecture-decisions.md")
+        .assert(predicates::path::exists());
 
     assert_eq!(
-        std::fs::read_to_string(format!("{}/.adr-dir", tmp_dir.path().to_str().unwrap())).unwrap(),
+        std::fs::read_to_string(format!("{}/.adr-dir", temp.path().to_str().unwrap())).unwrap(),
         "doc/adr"
     );
 }
@@ -28,54 +26,52 @@ fn test_init_default() {
 #[test]
 #[serial_test::serial]
 fn test_init_with_directory() {
-    let tmp_dir = TempDir::new().unwrap();
-    std::env::set_current_dir(tmp_dir.path()).unwrap();
+    let temp = TempDir::new().unwrap();
+    std::env::set_current_dir(temp.path()).unwrap();
 
     Command::cargo_bin("adrs")
         .unwrap()
         .arg("init")
-        .arg(tmp_dir.path())
+        .arg(temp.path())
         .assert()
         .success();
 
-    assert!(Path::new(tmp_dir.path())
-        .join("0001-record-architecture-decisions.md")
-        .exists());
+    temp.child("0001-record-architecture-decisions.md")
+        .assert(predicates::path::exists());
 
     assert_eq!(
-        std::fs::read_to_string(format!("{}/.adr-dir", tmp_dir.path().to_str().unwrap())).unwrap(),
-        tmp_dir.path().to_str().unwrap()
+        std::fs::read_to_string(format!("{}/.adr-dir", temp.path().to_str().unwrap())).unwrap(),
+        temp.path().to_str().unwrap()
     );
 }
 
 #[test]
 #[serial_test::serial]
 fn test_init_with_file_already_in_directory() {
-    let tmp_dir = TempDir::new().unwrap();
-    std::env::set_current_dir(tmp_dir.path()).unwrap();
+    let temp = TempDir::new().unwrap();
+    std::env::set_current_dir(temp.path()).unwrap();
 
     std::fs::OpenOptions::new()
         .create(true)
         .write(true)
         .open(format!(
             "{}/0001-record-architecture-decisions.md",
-            tmp_dir.path().to_str().unwrap()
+            temp.path().to_str().unwrap()
         ))
         .unwrap();
 
     Command::cargo_bin("adrs")
         .unwrap()
         .arg("init")
-        .arg(tmp_dir.path())
+        .arg(temp.path())
         .assert()
         .success();
 
-    assert!(Path::new(tmp_dir.path())
-        .join("0002-record-architecture-decisions.md")
-        .exists());
+    temp.child("0002-record-architecture-decisions.md")
+        .assert(predicates::path::exists());
 
     assert_eq!(
-        std::fs::read_to_string(format!("{}/.adr-dir", tmp_dir.path().to_str().unwrap())).unwrap(),
-        tmp_dir.path().to_str().unwrap()
+        std::fs::read_to_string(format!("{}/.adr-dir", temp.path().to_str().unwrap())).unwrap(),
+        temp.path().to_str().unwrap()
     );
 }
