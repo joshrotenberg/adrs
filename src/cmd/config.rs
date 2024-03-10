374834d3de427849b4fed31a862de9520df4c64c
josh rotenberg <joshrotenberg@gmail.com>
2024-03-09 17:20:54 -0800

    test  config

--- src/cmd/config.rs
+++ src/cmd/config.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use clap::Args;
 
+use crate::adr::read_adr_dir_file;
+
 #[derive(Debug, Args)]
 pub(crate) struct ConfigArgs {}
 
@@ -10,5 +12,9 @@ pub(crate) fn run(_args: &ConfigArgs) -> Result<()> {
         std::env::current_exe().unwrap().parent().unwrap().display()
     );
     println!("adrs_template_dir=embedded");
+    if let Ok(adr_dir) = read_adr_dir_file() {
+        println!("adrs_dir={}", adr_dir.display());
+        return Ok(());
+    }
     Ok(())
 }
--- tests/test_config.rs
+++ tests/test_config.rs
@@ -1,1 +1,16 @@
+use assert_cmd::Command;
+use assert_fs::TempDir;
+use predicates::prelude::*;
 
+#[test]
+#[serial_test::serial]
+fn test_config() {
+    let temp = TempDir::new().unwrap();
+    std::env::set_current_dir(temp.path()).unwrap();
+
+    Command::cargo_bin("adrs")
+        .unwrap()
+        .arg("config")
+        .assert()
+        .stdout(predicate::str::contains("adrs_bin_dir=").and(predicate::str::contains("adrs_template_dir=embedded")));
+}
