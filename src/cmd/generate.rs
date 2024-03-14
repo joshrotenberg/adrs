35574f2a1b39339a2f49d08ff433b52a7ea47969
josh rotenberg <joshrotenberg@gmail.com>
2024-03-13 18:13:16 -0700

    remove leading . if any

--- src/cmd/generate.rs
+++ src/cmd/generate.rs
@@ -72,6 +72,9 @@ fn run_graph(args: &GraphArgs) -> Result<()> {
     let adr_dir = read_adr_dir_file()?;
     let adrs = list_adrs(Path::new(&adr_dir))?;
 
+    let extension = args
+        .extension
+        .trim_start_matches(|c| char::is_ascii_punctuation(&c));
     let items = adrs
         .into_iter()
         .map(|path| {
@@ -86,7 +89,7 @@ fn run_graph(args: &GraphArgs) -> Result<()> {
     println!("digraph {{\n  node [shape=plaintext]\n  subgraph {{");
     for (number, title, filename, _links) in &items {
         let mut path = PathBuf::from(&filename);
-        path.set_extension(args.extension.as_str());
+        path.set_extension(extension);
 
         path = match &args.prefix {
             Some(prefix) => PathBuf::from(prefix).join(path),
